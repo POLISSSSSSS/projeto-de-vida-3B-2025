2517e1f445feadbeb2d71ad4aae1661f43b0d20a
Apolo <apolo.branco@escola.pr.gov.br>
2025-06-04 11:35:24 -0300

    04/06

--- main.js
+++ main.js
@@ -27,7 +27,11 @@ function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = tempoFinal / 1000;
-    return segundos;
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+    return dias + " dias " + horas " horas " + minutos " minutos " + segundos + " segundos";
+}
 
     segundos %= 60;
     minutos %= 60;
