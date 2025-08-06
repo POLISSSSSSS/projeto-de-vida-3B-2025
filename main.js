a547e5bd1082a8485ad38046eb3f7b72862f30d0
Apolo <apolo.branco@escola.pr.gov.br>
2025-08-06 11:20:17 -0300

    06/08

--- main.js
+++ main.js
@@ -50,10 +50,13 @@ function calculaTempo(tempoObjetivo) {
 
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
-        document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+        document.getElementById("dias" + i).textContent = calculaTempo(tempos[0])[0];
+
+        document.getElementById("horas" + i).textContent = calculaTempo(tempos[])[1];
+
+        document.getElementById("min" + i).textContent = calculaTempo(tempos[0])[2];
+        
+        document.getElementById("seg" + i).textContent = calculaTempo(tempos[0])[3];
     }
 }
 
